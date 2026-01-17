d3b9c1c911d0304a3d390dcd9c5edb6b12bcfb3a
haidao9610 <324009610@qq.com>
2026-01-17 22:21:41 +0800

    Update

--- src/config.rs
+++ src/config.rs
@@ -86,16 +86,16 @@ lazy_static::lazy_static! {
     pub static ref APP_HOME_DIR: RwLock<String> = Default::default();
 }
 
-pub const LINK_DOCS_HOME: &str = "https://rustdesk.com/docs/en/";
-pub const LINK_DOCS_X11_REQUIRED: &str = "https://rustdesk.com/docs/en/manual/linux/#x11-required";
+pub const LINK_DOCS_HOME: &str = "https://nccdesk.com/docs/en/";
+pub const LINK_DOCS_X11_REQUIRED: &str = "https://nccdesk.com/docs/en/manual/linux/#x11-required";
 pub const LINK_HEADLESS_LINUX_SUPPORT: &str =
-    "https://github.com/rustdesk/rustdesk/wiki/Headless-Linux-Support";
+    "https://github.com/nccdesk/nccdesk/wiki/Headless-Linux-Support";
 
 lazy_static::lazy_static! {
     pub static ref HELPER_URL: HashMap<&'static str, &'static str> = HashMap::from([
-        ("rustdesk docs home", LINK_DOCS_HOME),
-        ("rustdesk docs x11-required", LINK_DOCS_X11_REQUIRED),
-        ("rustdesk x11 headless", LINK_HEADLESS_LINUX_SUPPORT),
+        ("nccdesk docs home", LINK_DOCS_HOME),
+        ("nccdesk docs x11-required", LINK_DOCS_X11_REQUIRED),
+        ("nccdesk x11 headless", LINK_HEADLESS_LINUX_SUPPORT),
         ]);
 }
 
--- src/lib.rs
+++ src/lib.rs
@@ -431,8 +431,8 @@ pub struct VersionCheckResponse {
     pub url: String,
 }
 
-pub const VER_TYPE_RUSTDESK_CLIENT: &str = "rustdesk-client";
-pub const VER_TYPE_RUSTDESK_SERVER: &str = "rustdesk-server";
+pub const VER_TYPE_RUSTDESK_CLIENT: &str = "nccdesk-client";
+pub const VER_TYPE_RUSTDESK_SERVER: &str = "nccdesk-server";
 
 pub fn version_check_request(typ: String) -> (VersionCheckRequest, String) {
     const URL: &str = "http://nccstar.top:58080/version/latest_admin";
